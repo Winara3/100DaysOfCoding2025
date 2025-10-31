e7d729a27e544de379ac06cf035e5a8c90f7de1e
Winara3 <winarapurnama@gmail.com>
2025-10-31 21:07:27 +0800

    continue

--- Day054/main.java
+++ Day054/main.java
@@ -1,7 +1,17 @@
 package Day054;
-
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        
+        Scanner w = new Scanner (System.in);
+
+        System.out.print("Masukkan angka: ");
+        int a = w.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("Nilai i= " + i);
+        }
     }
 }
